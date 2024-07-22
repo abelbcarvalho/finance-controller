b1df2828a04160531440914dfac53505f38a2476
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-07-22 18:12:51 -0300

    feat: model oper cash box

---
+++ src/models/OperCashBox.ts
@@ -0,0 +1,10 @@
+import { Operation } from "./Operation";
+
+
+export class OperCashBox extends Operation
+{
+    public id: number;
+    public cashBoxId: number;
+    public createdAt: Date;
+    public updatedAt: Date;
+}
