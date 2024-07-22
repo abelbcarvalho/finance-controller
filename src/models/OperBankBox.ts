8ba0f220e0459036103479eec8d3fe4de66668a5
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-07-22 18:09:25 -0300

    feat: model oper bank box

---
+++ src/models/OperBankBox.ts
@@ -0,0 +1,10 @@
+import { Operation } from "./Operation";
+
+
+export class OperBankBox  extends Operation
+{
+    public id: number;
+    public bankBoxId: number;
+    public createdAt: Date;
+    public updatedAt: Date;
+}
