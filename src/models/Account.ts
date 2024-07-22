4fdc5027f64daf77645f1b2a17f8b4ad06075a45
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-07-22 10:30:39 -0300

    feat: model account class created

---
+++ src/models/Account.ts
@@ -0,0 +1,9 @@
+import { GenderEnum } from "../utilities/enums/EnumGender";
+
+export class Account
+{
+    public fullname: string;
+    public dateBorn: Date;
+    public documentCpf: string;
+    public gender: GenderEnum;
+}
