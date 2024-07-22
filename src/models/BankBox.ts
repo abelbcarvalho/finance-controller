668327e731a686f54da5a20423faaeaca4b16995
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-07-22 10:57:01 -0300

    feat: model bank box class created

---
+++ src/models/BankBox.ts
@@ -0,0 +1,11 @@
+export class BankBox
+{
+    public id: number;
+    public bankId: number;
+    public descript: string;
+    public balance: number;
+    public goal?: number;
+    public espireDate?: Date;
+    public createdAt: Date;
+    public updatedAt: Date;
+}
