a7ad265a66ff729c89abd0f4dbd3dff3843cf7c0
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-07-22 12:42:31 -0300

    feat: model cash box created

---
+++ src/models/CashBox.ts
@@ -0,0 +1,11 @@
+export class CashBox
+{
+    public id: number;
+    public cashId: number;
+    public descript: string;
+    public balance: number;
+    public goal?: number;
+    public expireDate?: number;
+    public createdAt: Date;
+    public updatedAt: Date;
+}
